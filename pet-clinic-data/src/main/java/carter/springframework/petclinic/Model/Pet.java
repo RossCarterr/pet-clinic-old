7f71c9ed07e288c1903b83a54cff417a7ef8a1b4
RossCarterr <ross29.8@hotmail.com>
2020-07-14 10:30:20 +0100

    adding base entity. Closes #10

--- pet-clinic-data/src/main/java/carter/springframework/petclinic/Model/Pet.java
+++ pet-clinic-data/src/main/java/carter/springframework/petclinic/Model/Pet.java
@@ -3,7 +3,7 @@ package carter.springframework.petclinic.Model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/carter/springframework/petclinic/Model/PetType.java
+++ pet-clinic-data/src/main/java/carter/springframework/petclinic/Model/PetType.java
@@ -1,6 +1,6 @@
 package carter.springframework.petclinic.Model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
